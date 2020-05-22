00f324a17253b65f62d5862aabc7c56837678525
Grant Montgomery <lettucebowler@gmail.com>
2020-05-22 17:12:51 -0500

    first working commit

--- nth-permutation.py
+++ nth-permutation.py
@@ -1,12 +1,26 @@
 #!/usr/bin/env python3
 import argparse
 from math import factorial
+from copy import deepcopy
 
 # Parser to get base number for computations
 parser = argparse.ArgumentParser(description='Finds the nth permutation of a string \
     when sorted alphabetically')
-parser.add_argument('-p', dest='string_to_permute', default='example', required=False)
+parser.add_argument('-p', dest='string_to_permute', default='default', required=False)
+parser.add_argument('-n', dest='n', default='1', required=False, type=int)
+
 args = parser.parse_args()
 initial_permutation = args.string_to_permute
+n = args.n
 
-start = [char for char in initial_permutation]
+alphabetical = sorted([char for char in initial_permutation])
+temp = deepcopy(alphabetical)
+nperm = []
+
+q = n - 1
+for x in range(len(alphabetical)):
+    index = q // factorial(len(temp) - 1) % len(temp)
+    nperm.append(temp[index])
+    temp.remove(temp[index])
+
+print(str(n) + 'th permutation: ' + ''.join(nperm))
