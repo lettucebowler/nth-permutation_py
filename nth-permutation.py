8deec3c076aa44a104392749136845a06bd68c4a
Grant Montgomery <lettucebowler@gmail.com>
2020-05-25 09:44:18 -0500

    working on duplicate handling

--- nth-permutation.py
+++ nth-permutation.py
@@ -3,24 +3,44 @@ import argparse
 from math import factorial
 from copy import deepcopy
 
+def ending(number):
+    num_ending = 'th'
+    if number == 1:
+        num_ending = 'st'
+    elif number == 2:
+        num_ending = 'nd'
+    elif number == 3:
+        num_ending = 'rd'
+    return num_ending
+
 # Parser to get base number for computations
 parser = argparse.ArgumentParser(description='Finds the nth permutation of a string \
     when sorted alphabetically')
 parser.add_argument('-p', dest='string_to_permute', default='default', required=False)
 parser.add_argument('-n', dest='n', default='1', required=False, type=int)
-
+parser.add_argument('-d', action='store_true')
 args = parser.parse_args()
 initial_permutation = args.string_to_permute
 n = args.n
+consider_duplicates = args.d
 
 alphabetical = sorted([char for char in initial_permutation])
 temp = deepcopy(alphabetical)
 nperm = []
 
+# Determine duplicate permutation handlilng based on user preference
+if consider_duplicates:
+    # Determine duplicate multiplier
+    duplicates = {i:alphabetical.count(i) for i in alphabetical}
+    multiplier = 1
+    for letter in duplicates:
+        multiplier *= factorial(duplicates[letter])
+    n *= multiplier
+
 q = n - 1
 for x in range(len(alphabetical)):
     index = q // factorial(len(temp) - 1) % len(temp)
     nperm.append(temp[index])
-    temp.remove(temp[index])
+    temp.remove(nperm[-1])
 
-print(str(n) + 'th permutation: ' + ''.join(nperm))
+print(str(n) + '{} permutation of {}: '.format(ending(n), initial_permutation) + ''.join(nperm))
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+for i in {1..24}
+do
+./nth-permutation.py -p aabb -n $i
+done
