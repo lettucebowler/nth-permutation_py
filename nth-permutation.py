74ba7342d3ccb8f481923b13957fc50558c7dd11
Grant Montgomery <lettucebowler@gmail.com>
2020-05-26 14:37:34 -0500

    reverted back to old functionality until I find a better way to not suck

--- nth-permutation.py
+++ nth-permutation.py
@@ -13,51 +13,6 @@ def ending(number):
         num_ending = 'rd'
     return num_ending
 
-def last_index_of(a, alphabetical):
-    return len(alphabetical) - list(reversed(alphabetical)).index(a)
-    
-
-def nth_perm(perm, chars, n, total):
-    # if len(perm) < total:
-    #     alphabetical = sorted([char for char in chars])
-    #     char_frequencies = {i:alphabetical.count(i) for i in alphabetical}
-    #     ranges = {a:factorial(len(chars) - 1) * char_frequencies[a] for a in char_frequencies}
-    #     for a in ranges:
-    #         if n < ranges[a]:
-    #             perm.append(a)
-    #             chars.remove(a)
-    #             break
-    #     perm = nth_perm(perm, chars, n, total)
-    # else:
-    #     print(str(perm))
-    #     return list(perm)
-    alphabetical = sorted([char for char in chars])
-    char_frequencies = {i:alphabetical.count(i) for i in alphabetical}
-    multiplier = 1
-    for i in char_frequencies:
-        multiplier *- char_frequencies[i]:
-    ranges = [multiplier * last_index_of(g, alphabetical) for g in char_frequencies]
-    while len(perm) < total:
-
-
-
-    
-
-
-# Determine duplicate permutation handlilng based on user preference
-
-    # Determine duplicate multiplier
-    # multiplier = 1
-    # for letter in duplicates:
-    #     multiplier *= factorial(duplicates[letter])
-    # n *= multiplier
-
-# q = n - 1
-# for x in range(len(alphabetical)):
-#     index = q // factorial(len(temp) - 1) % len(temp)
-#     nperm.append(temp[index])
-#     temp.remove(nperm[-1])
-
 # Parser to get base number for computations
 parser = argparse.ArgumentParser(description='Finds the nth permutation of a string \
     when sorted alphabetically')
@@ -72,7 +27,24 @@ if n > factorial(len(initial_permutation)):
     exit()
 
 # Driver Code
+alphabetical = sorted([char for char in initial_permutation])
+temp = deepcopy(alphabetical)
 nperm = []
-chars = [char for char in initial_permutation]
-perm = nth_perm(nperm, chars, n - 1, len(chars))
-print('{}{} permutation of {}: {}'.format(str(n), ending(n), initial_permutation, ''.join(perm)))
+
+# Determine duplicate multiplier
+char_frequencies = {i:alphabetical.count(i) for i in alphabetical}
+multiplier = 1
+for letter in char_frequencies:
+    multiplier *= factorial(char_frequencies[letter])
+if multiplier != 1:
+    print('This program cannot yet handle duplicates in the input string')
+    exit()
+
+# Generate nth-permutation
+q = n - 1
+for x in range(len(alphabetical)):
+    index = q // factorial(len(temp) - 1) % len(temp)
+    nperm.append(temp[index])
+    temp.remove(nperm[-1])
+
+print('{}{} permutation of {}: {}'.format(str(n), ending(n), initial_permutation, ''.join(nperm)))
